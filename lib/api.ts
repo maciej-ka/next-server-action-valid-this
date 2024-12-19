fb0f1335c7669a845241bc0a7150f3d1b60bc294
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-12-19 23:08:24 +0100

    feat: debug this on older next version

--- lib/api.ts
+++ lib/api.ts
@@ -4,8 +4,10 @@ import db from "./db";
 
 export const createItem = async () => {
   return new Promise<number>((resolve, reject) => {
+    console.dir(this);
     db.run('INSERT INTO items (title) VALUES (?)', ["foo"], function(err) {
       if (err) reject(err)
+      console.dir(this);
       resolve(this.lastID)
     })
   });
